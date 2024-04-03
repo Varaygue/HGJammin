a81ed833876cfbbf5ff3d6252d8da6eedeb31827
EvanVa1 <100057525+EvanVa1@users.noreply.github.com>
2024-04-03 15:14:29 -0400

    finished battery collection proto
    - added battery object
    - collision detection for picking up battery
    - ui number to show how many batteries collected

--- Assets/Directory/Scripts/PlayerCollisions.cs
+++ Assets/Directory/Scripts/PlayerCollisions.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerCollisions : MonoBehaviour
 {
 
     [SerializeField] Player_SO PlayerSO;
 
+    public static Action<int> batteryCollectedEvent;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Battery")
         {
             PlayerSO.batteries += 1;
+            batteryCollectedEvent?.Invoke(PlayerSO.batteries);
             Destroy(other.gameObject);
         }
     }
--- Assets/Directory/Scripts/Player_SO.cs
+++ Assets/Directory/Scripts/Player_SO.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Player_SO : ScriptableObject
 {
     public float progression;
-    public float batteries;
+    public int batteries;
 
 
 
